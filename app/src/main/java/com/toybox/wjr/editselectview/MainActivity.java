9992af9ce04c6b7fb98ddf03b9ba980d6fa96c28
wenjiarong <wenjiarong@bailun.com>
2018-08-04 15:01:37 +0800

    first commit

---
+++ app/src/main/java/com/toybox/wjr/editselectview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.toybox.wjr.editselectview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
